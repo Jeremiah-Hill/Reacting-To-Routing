afdb395a30f76b75e6dca3ea597f380c246b560a
Jeremiah-Hill <77300280+Jeremiah-Hill@users.noreply.github.com>
2021-03-02 15:41:54 -0600

    Add files via upload

---
+++ src/components/Films.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Films extends Component {
+  constructor() {
+    super();
+    this.state = {
+      films: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then((res) => res.json())
+      .then((films) => this.setState({ films }));
+  }
+  render() {
+    return (
+      <div className="row">
+        {this.state.films.map((film, id) => {
+          return (
+            <div className="card mx-5 my-2" style={{ width: "18rem" }} key={id}>
+              <div className="card-body">
+                <h5 className="card-title">{film.title}</h5>
+                <p className="card-text">{film.release_date}</p>
+
+                <Link to={`/films/${film.id}`}>
+                  <button className="btn btn-primary">Show more</button>
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
